e2f8b76020b78592f27adcdb936c8d5c87419e1e
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-06-21 16:27:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { StreamChat } from "stream-chat";
+import { useEffect, useState } from "react";
+import {
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  Chat,
+  MessageInput,
+  MessageList,
+  Window,
+} from "stream-chat-react";
+import "@stream-io/stream-chat-css/dist/css/index.css";
+
+const client = StreamChat.getInstance("fmsapnk8c76u");
 
 function App() {
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      await client.setGuestUser({
+        id: String(Math.floor(Math.random() * Date.now())),
+        name: "Anonymous",
+      });
+      const channel = await client.channel("tinubee", "three", {
+        name: "Third Channel",
+      });
+      setChannel(channel);
+      console.log(client);
+    })();
+    return () => {
+      client.disconnectUser();
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chat client={client}>
+      <ChannelList />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+      </Channel>
+    </Chat>
   );
 }
 
